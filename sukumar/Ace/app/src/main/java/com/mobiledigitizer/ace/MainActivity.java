9b9cc43f28b7a14f97accb406bb65756f4501fb1
sukumargaonkar <sukumargaonkar@gmail.com>
2016-03-17 08:36:03 +0530

    not working

--- sukumar/Ace/app/src/main/java/com/mobiledigitizer/ace/MainActivity.java
+++ sukumar/Ace/app/src/main/java/com/mobiledigitizer/ace/MainActivity.java
@@ -26,7 +26,7 @@ import org.opencv.imgproc.Imgproc;
 public class MainActivity extends Activity implements CvCameraViewListener2 {
 
     private CameraBridgeViewBase   	mOpenCvCameraView;
-    Mat 							rgba,gray,templateMat,templateMatBig,templateDescriptors,templateCanny,output;
+    Mat 							rgba,rgbaHolder,gray,templateMat,templateMatBig,templateDescriptors,templateCanny,output;
     MatOfKeyPoint 					templateKeypoints;
     String PicturesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
     public native void imageProcess(long matAddrRgba,long matAddrGray,long templateMat,long templateKeypoints,long templateDescriptors,long addrOutput);
@@ -44,14 +44,14 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                     System.loadLibrary("AceNativeLib");
                     mOpenCvCameraView.enableView();
 
-                    FeatureDetector detector = FeatureDetector.create(FeatureDetector.SURF);
-                    DescriptorExtractor descriptor = DescriptorExtractor.create(DescriptorExtractor.SIFT) ;
+                    FeatureDetector detector = FeatureDetector.create(FeatureDetector.SIFT);
+                    DescriptorExtractor descriptor = DescriptorExtractor.create(DescriptorExtractor.SURF) ;
                     templateKeypoints = new MatOfKeyPoint();
                     templateDescriptors = new Mat();
                     templateCanny = new Mat();
                     templateMat = new Mat();
-                    templateMatBig = Highgui.imread(PicturesDir+"/templateTestForm.jpg",Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-
+                    templateMatBig = Highgui.imread(PicturesDir+"/templateICCPCT.jpg",Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+/*
                     if(templateMatBig.height() > templateMatBig.width())
                     {
                         templateMatBig = templateMatBig.t();
@@ -59,18 +59,29 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                     }
                     int h =(int)( (double)templateMatBig.height()/templateMatBig.width() * 1920);
                     Imgproc.resize(templateMatBig, templateMat, new Size(1920, h));
-
+*/
 //                    Log.e("info", PicturesDir+"/formFront1Min.jpg");
 //                    long startTime = System.nanoTime();
 //                    Imgproc.Canny(templateMat, templateCanny, 50, 150);
-                    templateCanny = templateMat;
-                    Highgui.imwrite(PicturesDir+"/canny.jpg", templateCanny);
+//                    templateCanny = templateMat;
+                    templateCanny = templateMatBig;
+//                    Highgui.imwrite(PicturesDir+"/canny.jpg", templateCanny);
                     detector.detect(templateCanny, templateKeypoints);
                     Log.e("info", templateKeypoints.size().toString());
 //                    long endTime = System.nanoTime(); Log.e("info", "Detection- " + (float)(endTime - startTime)/1E9);
 //                    startTime = System.nanoTime();
                     descriptor.compute(templateCanny, templateKeypoints, templateDescriptors);
 //                    endTime = System.nanoTime(); Log.e("info", "Description- " + (float)(endTime - startTime)/1E9);
+
+                    {
+                        output = new Mat();
+                        rgbaHolder = Highgui.imread(PicturesDir+"/frameICCPCT.jpg",Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+                        Highgui.imwrite(PicturesDir+"/inputOriginal.jpg", rgbaHolder);
+                        imageProcess(rgbaHolder.getNativeObjAddr(),rgbaHolder.getNativeObjAddr(),templateCanny.getNativeObjAddr(),templateKeypoints.getNativeObjAddr(),templateDescriptors.getNativeObjAddr(),output.getNativeObjAddr());
+                        Highgui.imwrite(PicturesDir + "/op.jpg", output);
+                        Highgui.imwrite(PicturesDir+"/op2.jpg", rgbaHolder);
+                    }
+
                 } break;
                 default:
                 {
@@ -105,11 +116,11 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         rgba = inputFrame.rgba();
         gray = inputFrame.gray();
 
-        long startTime = System.nanoTime();
-        imageProcess(rgba.getNativeObjAddr(),gray.getNativeObjAddr(),templateCanny.getNativeObjAddr(),templateKeypoints.getNativeObjAddr(),templateDescriptors.getNativeObjAddr(),output.getNativeObjAddr());
-        long endTime = System.nanoTime();
-        Log.e("info", "Native Code Time- " + (float)(endTime - startTime)/1E9);
-        Highgui.imwrite(PicturesDir+"/op.jpg", output);
+//        long startTime = System.nanoTime();
+//        imageProcess(rgba.getNativeObjAddr(),gray.getNativeObjAddr(),templateCanny.getNativeObjAddr(),templateKeypoints.getNativeObjAddr(),templateDescriptors.getNativeObjAddr(),output.getNativeObjAddr());
+//        long endTime = System.nanoTime();
+//        Log.e("info", "Native Code Time- " + (float)(endTime - startTime)/1E9);
+//        Highgui.imwrite(PicturesDir+"/op.jpg", output);
 
         return rgba;
     }
--- sukumar/Ace/app/src/main/jni/AceJNI.cpp
+++ sukumar/Ace/app/src/main/jni/AceJNI.cpp
@@ -18,6 +18,7 @@ using namespace cv;
 extern "C" {
 JNIEXPORT void JNICALL Java_com_mobiledigitizer_ace_MainActivity_imageProcess(JNIEnv* env,jobject thisObj,jlong addrRgba,jlong addrGray,jlong addrTemplateMat,jlong addrTemplateKeypoints,jlong addrTemplateDescriptors,jlong addrOutput);
 void Mat_to_vector_KeyPoint(Mat& mat, vector<KeyPoint>& v_kp);
+bool comapreDMatch(DMatch match1,DMatch match2);
 
 JNIEXPORT void JNICALL Java_com_mobiledigitizer_ace_MainActivity_imageProcess(JNIEnv* env,jobject thisObj,jlong addrRgba,jlong addrGray,jlong addrTemplateMat,jlong addrTemplateKeypoints,jlong addrTemplateDescriptors,jlong addrOutput){
 //	__android_log_print(ANDROID_LOG_ERROR, "jni", "Entered Native Code2.0");
@@ -51,13 +52,13 @@ JNIEXPORT void JNICALL Java_com_mobiledigitizer_ace_MainActivity_imageProcess(JN
 	Mat_to_vector_KeyPoint(templateKeypointMat,templateKeypoints);
 //	__android_log_print(ANDROID_LOG_ERROR, "info", "MatofKeypoint to vector - %fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
-//    SiftFeatureDetector detector;
+    SiftFeatureDetector detector;
 //    OrbFeatureDetector detector;
 //    OrbDescriptorExtractor descriptor;
 //    FastFeatureDetector detector;
-    SurfFeatureDetector detector;
-    SiftDescriptorExtractor descriptor;
-//    SurfDescriptorExtractor descriptor;
+//    SurfFeatureDetector detector;
+//    SiftDescriptorExtractor descriptor;
+    SurfDescriptorExtractor descriptor;
 //    BriefDescriptorExtractor descriptor;
 
 	std::vector<KeyPoint> keypoints_1;
@@ -78,27 +79,29 @@ JNIEXPORT void JNICALL Java_com_mobiledigitizer_ace_MainActivity_imageProcess(JN
 
 		for (size_t i = 0; i < contours.size(); i++) {
 			approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
-			doubleHolder = contourArea(contours_poly[i]);
+			doubleHolder = contourArea(contours[i]);
 			if (doubleHolder > maxBoundryArea) {
 				maxBoundryArea = doubleHolder;
 				formContourIndex = i;
 			}
 		}
-		boundRect = boundingRect(Mat(contours_poly[formContourIndex]));
+		boundRect = boundingRect(Mat(contours[formContourIndex]));
 		if(maxBoundryArea > 2) {
 			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-			rectangle(rgba, boundRect.tl(), boundRect.br(), color, 2, 8, 0);
+			rectangle(gray, boundRect.tl(), boundRect.br(), color, 2, 8, 0);
 		}
 	}
-//	__android_log_print(ANDROID_LOG_ERROR, "info", "Contour count: %d  Area: %f",contours.size(),maxBoundryArea);
+	__android_log_print(ANDROID_LOG_ERROR, "info", "Contour count: %d  Area: %f",contours.size(),maxBoundryArea);
 
 	if(contours.size() > 1)
-		gray = gray(boundRect);
+        gray = gray(boundRect);
 
+/*
 
 	tStart = clock();
 	detector.detect(gray,keypoints_1);
 //	__android_log_print(ANDROID_LOG_ERROR, "info", "Keypoint detection time- %fs Keypoints- %d\n", (double)(clock() - tStart)/CLOCKS_PER_SEC,keypoints_1.size());
+	__android_log_print(ANDROID_LOG_ERROR, "info", "Keypoints- %d\n",keypoints_1.size());
 
 	tStart = clock();
 	descriptor.compute(gray,keypoints_1,descriptors_1);
@@ -119,12 +122,22 @@ JNIEXPORT void JNICALL Java_com_mobiledigitizer_ace_MainActivity_imageProcess(JN
 	tStart = clock();
 	matcher.match( descriptors_1, templateDescriptors, matches );
 //	__android_log_print(ANDROID_LOG_ERROR, "info", "Matcher- %fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+	__android_log_print(ANDROID_LOG_ERROR, "info", "Matches Count : %d\n", matches.size());
 
-	double max_dist = 0; double min_dist = 1000;
+    double max_dist = 0; double min_dist = 1000;
 	DMatch match;
 	int goodMatchCount = 0;
 	double AvgError = 0;
 	std::vector< DMatch > good_matches;
+
+    sort(matches.begin(),matches.end(),comapreDMatch);
+    for(int i=0;i<min(10,(int)matches.size());i++)
+    {
+        goodMatchCount = 10;
+        match = matches[i];
+        __android_log_print(ANDROID_LOG_ERROR, "info", "Distance : %f\n", match.distance);
+        good_matches.push_back(match);
+    }
 
 	for( int i = 0; i < matches.size(); i++ )
 	{
@@ -148,7 +161,7 @@ JNIEXPORT void JNICALL Java_com_mobiledigitizer_ace_MainActivity_imageProcess(JN
 		{
 			goodMatchCount++;
 			AvgError += match.distance;
-			good_matches.push_back(match);
+			//good_matches.push_back(match);
 			keypointHolder = keypoints_1[match.queryIdx];
 			goodFrameKeypoints.push_back(keypointHolder.pt);
 			keypointHolder = templateKeypoints[match.trainIdx];
@@ -160,12 +173,41 @@ JNIEXPORT void JNICALL Java_com_mobiledigitizer_ace_MainActivity_imageProcess(JN
 	__android_log_print(ANDROID_LOG_ERROR, "info", "min - %f max - %f\n",min_dist,max_dist);
 
 	__android_log_print(ANDROID_LOG_ERROR, "info", "Good Matches - %d Avg Error- %f\n",goodMatchCount, AvgError);
-//	drawKeypoints( grayCanny, goodFrameKeypoints, grayCanny, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+*/
+ //	drawKeypoints( grayCanny, goodFrameKeypoints, grayCanny, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
 //	drawKeypoints( templateImgGray, goodTemplateKeypoints, templateImgGray, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
 //    gray = gray(boundRect);
-	drawMatches( gray, keypoints_1, templateImgGray, templateKeypoints,good_matches, output, Scalar::all(-1), Scalar::all(-1),vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+//	drawMatches( gray, keypoints_1, templateImgGray, templateKeypoints,good_matches, output, Scalar::all(-1), Scalar::all(-1),vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
+///*************************************************
+    std::vector<Point> templateCorners(4);
+    std::vector<Point> frameCorners = contours_poly[formContourIndex];
+    templateCorners[0] = Point(0,0); templateCorners[1] = Point( templateImgGray.cols, 0 );
+    templateCorners[2] = Point( templateImgGray.cols, templateImgGray.rows ); templateCorners[3] = Point( 0, templateImgGray.rows );
+    __android_log_print(ANDROID_LOG_ERROR, "info", "template - %d frame- %d\n",templateCorners.size(), frameCorners.size());
+    Mat H = findHomography( templateCorners, frameCorners, CV_RANSAC );
+//	136,515 1410,526 1388,1165 121,1154
+	std::vector<Point2f> boxOriginal(4);
+	std::vector<Point2f> scene_corners(4);
+	boxOriginal[0] = Point(515,136);
+	boxOriginal[1] = Point(526,1410);
+	boxOriginal[2] = Point(1165,1388);
+	boxOriginal[3] = Point(1154,121);
 
+	perspectiveTransform( boxOriginal, scene_corners, H);
+	//-- Draw lines between the corners (the mapped object in the scene - image_2 )
+	Point2f point0 = scene_corners[0];
+	Point2f point1 = scene_corners[1];
+	Point2f point2 = scene_corners[2];
+	Point2f point3 = scene_corners[3];
+	line( gray, point0 + Point2f( templateImgGray.cols, 0), point1 + Point2f( templateImgGray.cols, 0), Scalar(0, 255, 0),10,8,0);
+	line( gray, point1 + Point2f( templateImgGray.cols, 0), point2 + Point2f( templateImgGray.cols, 0), Scalar( 0, 255, 0), 10 ,8,0);
+	line( gray, point2 + Point2f( templateImgGray.cols, 0), point3 + Point2f( templateImgGray.cols, 0), Scalar( 0, 255, 0), 10,8,0 );
+	line( gray, point3 + Point2f( templateImgGray.cols, 0), point0 + Point2f( templateImgGray.cols, 0), Scalar( 0, 255, 0), 10 ,8,0);
+
+ //*****************************************************/
+
+/*
 	if(goodTemplateKeypoints.size() > 3 && goodFrameKeypoints.size() > 3)
 	{
 		Mat H = findHomography( goodTemplateKeypoints, goodFrameKeypoints, CV_RANSAC );
@@ -187,8 +229,9 @@ JNIEXPORT void JNICALL Java_com_mobiledigitizer_ace_MainActivity_imageProcess(JN
 		line( rgba, point3 + Point2f( templateImgGray.cols, 0), point0 + Point2f( templateImgGray.cols, 0), Scalar( 0, 255, 0), 10 ,8,0);
 //		__android_log_print(ANDROID_LOG_ERROR, "info", "%f-%f %f-%f",point0.x,point0.y,point1.x,point1.y);
 	}
-
+*/
 //    rgba = grayCannyHolder;
+
 }
 
 void Mat_to_vector_KeyPoint(Mat& mat, vector<KeyPoint>& v_kp)
@@ -201,6 +244,11 @@ void Mat_to_vector_KeyPoint(Mat& mat, vector<KeyPoint>& v_kp)
 		v_kp.push_back(kp);
 	}
 	return;
+}
+
+bool comapreDMatch(DMatch match1,DMatch match2)
+{
+    return match1.distance < match2.distance;
 }
 
 }
